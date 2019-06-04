a9b9f0220fd9c9bb1ba12cbca35a1504aa2d1530
Fernanda Salas <mfssanhueza@gmail.com>
2019-06-04 12:05:55 -0400

    List_todos listo

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,22 +1,20 @@
 class TodosController < ApplicationController
   def index
     @todos = Todo.all
-
   end
 
   def new
     @todo = Todo.new
-
   end
 
   def create
     @todo = Todo.new(todo_params)
     redirect_to todos_path if @todo.save
-
   end
 
   def show
       @todo = Todo.find(params[:id])
+      @todo.completed ? @status = 'Realizada' : @status = 'Pendiente'
   end
 
   def edit
@@ -34,6 +32,22 @@ class TodosController < ApplicationController
     @todo.delete
     redirect_to todos_path
 
+  end
+
+  def complete
+    @todo = Todo.find(params[:id])
+    @todo.completed = true
+    @todo.save
+    redirect_to todos_path
+  end
+
+  def list
+    @done = []
+    @incomplete = []
+    @todos = Todo.all
+    @todos.each do |todo|
+      todo.completed ? @done.push(todo) : @incomplete.push(todo)
+    end
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,10 @@ Rails.application.routes.draw do
   get '/todos', to: 'todos#index'
   get 'todos/new'
   post '/todos', to: 'todos#create'
+  get 'todos/list'
   get '/todos/:id', to: 'todos#show', as: 'todo'
   get '/todos/:id/edit', to:'todos#edit', as: 'todos_edit'
   patch '/todos/:id', to: 'todos#update'
   delete 'todos/:id', to:'todos#destroy', as: 'todos_destroy'
+  get '/todos/:id/complete', to: 'todos#complete', as: 'todos_complete'
 end
