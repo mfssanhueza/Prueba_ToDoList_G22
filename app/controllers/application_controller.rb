dbd87755c8ff4ab882ff56cbdcc0a88aba353eb9
Fernanda Salas <mfssanhueza@gmail.com>
2019-06-04 11:01:26 -0400

    Functional index, new and create

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,23 @@
 class ApplicationController < ActionController::Base
+  def index
+    @todos = Todo.all
+
+  end
+
+  def new
+    @todo = Todo.new
+
+  end
+
+  def create
+    @todo = Todo.new(todo_params)
+    redirect_to todos_path if @todo.save
+
+  end
+
+  private
+  def todo_params
+    params.require(:todo).permit(:description, :completed)
+
+  end
 end
